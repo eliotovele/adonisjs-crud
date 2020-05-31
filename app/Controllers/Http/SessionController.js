67e2af6f6e123101fdfba5929cc9f8e8752defca
eliotovele <eliotovele@protonmail.com>
2020-05-31 13:36:55 +0200

    User Login

---
+++ app/Controllers/Http/SessionController.js
@@ -0,0 +1,17 @@
+'use strict'
+
+class SessionController {
+    async store({request,response,auth}){
+        const {email, password} = request.all()
+
+        try {
+            const token = await auth.attempt(email,password )
+
+            return token
+        } catch (error) {
+            return response.status(404).send('User not found')
+        }
+    }
+}
+
+module.exports = SessionController
